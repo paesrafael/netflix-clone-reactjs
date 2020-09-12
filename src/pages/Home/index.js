a11b4c40b682d19830405c5f75d09fa624b01db8
Rafael Paes <rfaelpaes@gmail.com>
2020-09-12 16:00:56 -0300

    feat: desenvolve a página Home

---
+++ src/pages/Home/index.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import MovieList from '~/components/MovieList'
+
+// import './styles.css'
+
+export default function Home() {
+  return (
+    <>
+      <MovieList />
+    </>
+  )
+}
