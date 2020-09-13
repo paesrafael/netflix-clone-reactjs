c852510af578a55087ee9e3406fedbe617d94db5
Rafael Paes <rfaelpaes@gmail.com>
2020-09-12 22:29:04 -0300

    refactor: adiciona o Header na página Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
+import Header from '~/components/Header'
 import MovieList from '~/components/MovieList'
 
 // import './styles.css'
 
 export default function Home() {
+  const [pageScroll, setPageScroll] = useState(false)
+
+  useEffect(() => {
+    function scrollListener() {
+      if (window.scrollY > 10) {
+        setPageScroll(true)
+      } else {
+        setPageScroll(false)
+      }
+    }
+
+    window.addEventListener('scroll', scrollListener)
+
+    return () => {
+      window.removeEventListener('scroll', scrollListener)
+    }
+  }, [])
+
   return (
     <>
+      <Header scroll={pageScroll} />
+
       <MovieList />
     </>
   )
