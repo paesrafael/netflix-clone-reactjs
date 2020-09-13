57d62f7ebc0a3652b3637a795be07339d513d0da
Rafael Paes <rfaelpaes@gmail.com>
2020-09-12 22:35:53 -0300

    feat: chama o FeaturedMovie com parametro no MovieList

--- src/components/MovieList/index.js
+++ src/components/MovieList/index.js
@@ -3,11 +3,13 @@ import React, { useEffect, useState } from 'react'
 import api from '~/services/api'
 
 import MovieListCard from '~/components/MovieListCard'
+import FeaturedMovie from '~/components/FeaturedMovie'
 
 import './styles.css'
 
 export default function MovieList() {
-  const [movieList, setMovieList] = useState([])
+  const [movieList, setMovieList] = useState(null)
+  const [featuredMovie, setFeaturedMovie] = useState(null)
 
   useEffect(() => {
     async function loadMovieList() {
@@ -19,18 +21,44 @@ export default function MovieList() {
     loadMovieList()
   }, [])
 
+  useEffect(() => {
+    async function loadFeaturedMovie() {
+      const movieOriginals = movieList.filter(
+        (feature) => feature.slug === 'originals'
+      )
+      const movieChosenRandom = Math.floor(
+        Math.random() * (movieOriginals[0].movies.results.length - 1)
+      )
+      const movieChosen = movieOriginals[0].movies.results[movieChosenRandom]
+      const chosenInformation = await api.getMovieInformation(
+        movieChosen.id,
+        'tv'
+      )
+
+      setFeaturedMovie(chosenInformation)
+    }
+
+    if (movieList !== null) {
+      loadFeaturedMovie()
+    }
+  }, [movieList])
+
   return (
-    <section className="movie-list-section">
-      {!!movieList &&
-        movieList.map((item, key) => (
-          <div key={key} className="movie-list-card">
-            <MovieListCard
-              title={item.title}
-              movies={item.movies}
-              slug={item.slug}
-            />
-          </div>
-        ))}
-    </section>
+    <>
+      {!!featuredMovie && <FeaturedMovie movie={featuredMovie} />}
+
+      <section className="movie-list-section">
+        {!!movieList &&
+          movieList.map((item, key) => (
+            <div key={key} className="movie-list-card">
+              <MovieListCard
+                title={item.title}
+                movies={item.movies}
+                slug={item.slug}
+              />
+            </div>
+          ))}
+      </section>
+    </>
   )
 }
